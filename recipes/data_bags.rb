be1b25a2632cc1fe28c3bab9c1db3ec5422612d2
Barreto <diabar@amazon.com>
2019-01-30 21:23:00 +1100

    change

--- recipes/data_bags.rb
+++ recipes/data_bags.rb
@@ -1,6 +1,10 @@
+Chef::Log.info("********** A **********")
 instance = search("aws_opsworks_instance").first
+Chef::Log.info("********** B **********")
 layer = search("aws_opsworks_layer").first
+Chef::Log.info("********** C **********")
 stack = search("aws_opsworks_stack").first
+Chef::Log.info("********** D: **********")
 
 Chef::Log.info("********** This instance's instance ID is '#{instance['instance_id']}' **********")
 Chef::Log.info("********** This instance's public IP address is '#{instance['public_ip']}' **********")
